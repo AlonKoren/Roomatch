f0bde1e6a1b9379db0e4a3fbee899b174c3c1f8d
Alon Koren <alonkoren312@gmail.com>
2020-05-01 18:30:30 +0300

    Creating a New Ionic Angular Project
    Adding & Loading a New Page 
    Setting Up Angular Routes
    Navigating Between Pages
    Deleting a Recipe

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {RecipesService} from './recipes.service';
+import {Recipe} from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
-
-  constructor() { }
+  recipes: Recipe[];
+  constructor(private recipesService: RecipesService) { }
 
   ngOnInit() {
+    this.recipes = this.recipesService.getAllRecipes();
   }
 
 }
