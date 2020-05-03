52d99550d200c1a632c858c1319918cf20f2ba1a
Alon Koren <alonkoren312@gmail.com>
2020-05-03 14:41:38 +0300

    Adding Navigations
    Forward and back

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {NavController} from '@ionic/angular';
+import {PlacesService} from '../../places.service';
+import {Place} from '../../place.model';
 
 @Component({
   selector: 'app-place-detail',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./place-detail.page.scss'],
 })
 export class PlaceDetailPage implements OnInit {
+    place: Place;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private navCtrl: NavController, private placesService: PlacesService) { }
 
   ngOnInit() {
+      this.route.paramMap.subscribe(paramMap => {
+          if (!paramMap.has('placeId')) {
+              this.navCtrl.navigateBack('/places/tabs/discover');
+              return;
+          }
+          this.place = this.placesService.getPlace(paramMap.get('placeId'));
+      });
   }
 
+   onBookPlace() {
+    // this.router.navigateByUrl('/places/tabs/discover');
+     this.navCtrl.navigateBack('/places/tabs/discover');
+   }
 }
---
+++ src/app/places/places.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import {Place} from './place.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+  // tslint:disable-next-line:variable-name
+  private _places: Place[]  = [
+      new Place(
+          'p1',
+          'Manhattan Mansion',
+          'In the heart of New York City.',
+          'https://www.filmlocationswanted.com/wp-content/uploads/2015/06/manhattan-estate-mansion60.jpg',
+          149.99
+      ),
+    new Place(
+          'p2',
+          'L\'Amour Toujours',
+          'A romantic place in Paris!',
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTiHqzuPiuIHSPSVhAh3TpQQbLAeYWX6aQiYQOZjvRHKgfYwhkE&usqp=CAU',
+          189.99
+      ),
+    new Place(
+          'p3',
+          'The Foggy Palace',
+          'Not your average city trip!',
+          'https://i.pinimg.com/originals/9c/88/44/9c8844b217bdb6c17db14f51ad2e51a5.jpg',
+          99.99
+      )
+  ];
+
+  get places() {
+    return [...this._places];
+  }
+
+  constructor() { }
+
+  getPlace(id: string) {
+    return {...this._places.find(p => p.id === id)};
+  }
+}
