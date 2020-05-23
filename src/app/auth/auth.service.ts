054af1771753fabb8430ac89303c396726f16031
Alon Koren <alonkoren312@gmail.com>
2020-05-23 17:51:23 +0300

    Adding a Booking

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,7 +7,7 @@ export class AuthService {
   // tslint:disable-next-line:variable-name
   private _userIsAuthenticated = true;
   // tslint:disable-next-line:variable-name
-  private _userId = 'abc';
+  private _userId = 'xyz';
 
   get userIsAuthenticated() {
     return this._userIsAuthenticated;
