9abf89ec88671e5466b8bda197a680d86866949c
Alon Koren <alonkoren312@gmail.com>
2020-05-23 18:25:42 +0300

    Fetching Bookings By User

--- src/app/bookings/booking.service.ts
+++ src/app/bookings/booking.service.ts
@@ -2,8 +2,20 @@ import {Injectable} from '@angular/core';
 import {Booking} from './booking.model';
 import {BehaviorSubject} from 'rxjs';
 import {AuthService} from '../auth/auth.service';
-import {delay, switchMap, take, tap} from 'rxjs/operators';
+import {delay, map, switchMap, take, tap} from 'rxjs/operators';
 import {HttpClient} from '@angular/common/http';
+
+interface BookingData {
+    bookedFrom: string;
+    bookedTo: string;
+    firstName: string;
+    guestNumber: number;
+    lastName: string;
+    placeId: string;
+    placeImage: string;
+    placeTitle: string;
+    userId: string;
+}
 
 @Injectable({ providedIn: 'root' })
 export class BookingService {
@@ -61,6 +73,38 @@ export class BookingService {
             delay(1000),
             tap(bookings => {
                 this._bookings.next(bookings.filter(b => b.id !== bookingId));
+            })
+        );
+    }
+
+    fetchBookings() {
+        return this.http.get<{ [key: string]: BookingData }>(
+            `https://ionic-angular-course-b87ef.firebaseio.com/bookings.json?orderBy="userId"&equalTo="${this.authService.userId}"`
+        ).pipe(
+            map(bookingData => {
+                const bookings = [];
+                for (const key in bookingData) {
+                    if (bookingData.hasOwnProperty(key)) {
+                        bookings.push(
+                            new Booking(
+                                key,
+                                bookingData[key].placeId,
+                                bookingData[key].userId,
+                                bookingData[key].placeTitle,
+                                bookingData[key].placeImage,
+                                bookingData[key].firstName,
+                                bookingData[key].lastName,
+                                bookingData[key].guestNumber,
+                                new Date(bookingData[key].bookedFrom),
+                                new Date(bookingData[key].bookedTo)
+                            )
+                        );
+                    }
+                }
+                return bookings;
+            }),
+            tap(bookings => {
+                this._bookings.next(bookings);
             })
         );
     }
