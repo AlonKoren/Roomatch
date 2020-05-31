f87990c3d14de6e325a0b22b538ef02392445888
Alon Koren <alonkoren312@gmail.com>
2020-06-01 00:56:02 +0300

    Adding a Filepicker Fallback

--- src/app/shared/pickers/image-picker/image-picker.component.ts
+++ src/app/shared/pickers/image-picker/image-picker.component.ts
@@ -1,5 +1,6 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 import {Plugins, Capacitor, CameraSource, CameraResultType} from '@capacitor/core';
+import {Platform} from '@ionic/angular';
 
 @Component({
   selector: 'app-image-picker',
@@ -7,14 +8,29 @@ import {Plugins, Capacitor, CameraSource, CameraResultType} from '@capacitor/cor
   styleUrls: ['./image-picker.component.scss'],
 })
 export class ImagePickerComponent implements OnInit {
+  @ViewChild('filePicker', { static: false }) filePickerRef: ElementRef<HTMLInputElement>;
   @Output() imagePick = new EventEmitter<string>();
   selectedImage: string;
-  constructor() { }
+  usePicker = false;
 
-  ngOnInit() {}
+  constructor(
+      private platform: Platform
+  ) {}
+
+  ngOnInit() {
+    console.log('Mobile:', this.platform.is('mobile'));
+    console.log('Hybrid:', this.platform.is('hybrid'));
+    console.log('IOS:', this.platform.is('ios'));
+    console.log('Android:', this.platform.is('android'));
+    console.log('Desktop:', this.platform.is('desktop'));
+    if ((this.platform.is('mobile') && !this.platform.is('hybrid')) || this.platform.is('desktop')) {
+      this.usePicker = true;
+    }
+  }
 
   onPickImage() {
-    if (!Capacitor.isPluginAvailable('Camera')) {
+    if (!Capacitor.isPluginAvailable('Camera') || this.usePicker) {
+      this.filePickerRef.nativeElement.click();
       return;
     }
     Plugins.Camera.getPhoto({
@@ -31,6 +47,10 @@ export class ImagePickerComponent implements OnInit {
       console.log(error);
       return false;
     });
+  }
+
+  onFileChosen(event: Event) {
+    console.log(event);
   }
 
 }
