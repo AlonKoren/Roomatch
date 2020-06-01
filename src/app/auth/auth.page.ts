77ff77bac03c256081d19b53d944393728c84365
Alon Koren <alonkoren312@gmail.com>
2020-06-01 23:02:54 +0300

    Adding Authentication - User Signup
    Adding User Signup

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -42,12 +42,14 @@ export class AuthPage implements OnInit {
     }
     const email = form.value.email;
     const password = form.value.password;
-    console.log(email, password);
 
     if (this.isLogin) {
        // send a request to login servers
     } else {
       // send a request to signup servers
+      this.authService.signup(email, password).subscribe(resData => {
+        console.log(resData);
+      });
     }
 
   }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+
+interface AuthResponseData {
+  kind:	string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  localId: string;
+  expiresIn: string;
+  registered?: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   // tslint:disable-next-line:variable-name
-  private _userIsAuthenticated = true;
+  private _userIsAuthenticated = false;
   // tslint:disable-next-line:variable-name
-  private _userId = 'xyz';
+  private _userId = null;
 
   get userIsAuthenticated() {
     return this._userIsAuthenticated;
@@ -17,7 +29,14 @@ export class AuthService {
     return this._userId;
   }
 
-  constructor() { }
+  constructor(
+      private http: HttpClient
+  ) {}
+
+  signup(email: string, password: string) {
+    return this.http.post<AuthResponseData>(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${
+      environment.firebaseAPIKey}`, {email, password, returnSecureToken: true});
+  }
 
   login() {
     this._userIsAuthenticated = true;
