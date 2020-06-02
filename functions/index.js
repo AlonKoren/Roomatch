dc24e8024dc9eeea4e8d21d0ca7ed9b0a57e1dda
Alon Koren <alonkoren312@gmail.com>
2020-06-02 15:58:41 +0300

    Requiring the Auth Token on the Backend

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,7 @@ const os = require('os');
 const path = require('path');
 const fs = require('fs');
 const { v4: uuid } = require('uuid');
+const fbAdmin = require('firebase-admin');
 
 const { Storage } = require('@google-cloud/storage');
 
@@ -14,11 +15,23 @@ const storage = new Storage({
     keyFilename: "./ionic-angular-admin-key.json"
 });
 
+fbAdmin.initializeApp({
+    credential: fbAdmin.credential.cert(require('./ionic-app.json'))
+});
+
 exports.storeImage = functions.https.onRequest((req, res) => {
     return cors(req, res, () => {
         if (req.method !== 'POST') {
             return res.status(500).json({ message: 'Not allowed.' });
         }
+
+        if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')){
+            return res.status(401).json({ error: 'Unauthorized!' });
+        }
+
+        let idToken;
+        idToken = req.headers.authorization.split('Bearer ')[1];
+
         const busboy = new Busboy({ headers: req.headers });
         let uploadData;
         let oldImagePath;
@@ -40,20 +53,24 @@ exports.storeImage = functions.https.onRequest((req, res) => {
                 imagePath = oldImagePath;
             }
 
-            return storage
-                .bucket(storage.projectId + '.appspot.com')
-                .upload(uploadData.filePath, {
-                    gzip: true,
-                    uploadType: 'media',
-                    destination: imagePath,
-                    metadata: {
-                        metadata: {
-                            contentType: uploadData.type,
-                            firebaseStorageDownloadTokens: id
-                        }
-                    }
+            return fbAdmin
+                .auth()
+                .verifyIdToken(idToken)
+                .then(decodedToken => {
+                    return storage
+                        .bucket(storage.projectId + '.appspot.com')
+                        .upload(uploadData.filePath, {
+                            gzip: true,
+                            uploadType: 'media',
+                            destination: imagePath,
+                            metadata: {
+                                metadata: {
+                                    contentType: uploadData.type,
+                                    firebaseStorageDownloadTokens: id
+                                }
+                            }
+                        })
                 })
-
                 .then(() => {
                     return res.status(201).json({
                         imageUrl:
