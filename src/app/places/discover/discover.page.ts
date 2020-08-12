953be9f0fa84e12143a8f40320ee5bdeb67b2cb1
Alon Koren <alonkoren312@gmail.com>
2020-08-13 01:36:24 +0300

    Connection to the server of the algorithm

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,12 +1,11 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {PlacesService} from '../places.service';
-import {Place} from '../place.model';
 import { SegmentChangeEventDetail } from '@ionic/core';
 import {Subscription} from 'rxjs';
 import {AuthService} from '../../auth/auth.service';
-import {take} from 'rxjs/operators';
+import {switchMap, take} from 'rxjs/operators';
 import {MyUser} from '../../auth/myUser.model';
 import {FavoriteService} from '../../favorites/favorite.service';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-discover',
@@ -14,60 +13,84 @@ import {FavoriteService} from '../../favorites/favorite.service';
   styleUrls: ['./discover.page.scss'],
 })
 export class DiscoverPage implements OnInit, OnDestroy {
-  loadedUsers: MyUser[];
-  listedLoadedUsers: MyUser[];
-  relevantUsers: MyUser[];
-  isLoading = false;
+  allUsers: MyUser[] = [];
+  loadedUsers: MyUser[] = [];
+  relevantUsers: MyUser[] = [];
+  favArray: string[] = []; // all fav users dynamic
+  isLoading = true;
+  isLoadingUsers = true;
   private usersSub: Subscription;
   private favoritesSub: Subscription;
-  favArray = []; // all fav users dynamic
+  private relativeUserIdsSubs: Subscription;
   mode = 'all';
+  listSize = 0;
 
-  constructor(private authService: AuthService, private favoriteService: FavoriteService) { }
+  constructor(private authService: AuthService,
+              private favoriteService: FavoriteService,
+              private http: HttpClient) {
+  }
 
   ngOnInit() {
-    this.usersSub = this.authService.users.subscribe(users => {
-      this.loadedUsers = users;
-
-      // TODO Filter loaded users by the algo
-      // TODO  by given myuserid to get all user ids the relative to me
-
-      this.onFilterUpdate(this.mode);
-    });
+    // console.log('ngOnInit');
+    // this.isLoading = true;
+    // this.isLoadingUsers = true;
     this.favoritesSub = this.favoriteService.favorites.subscribe(value => {
       this.favArray = value.map(value1 => value1.favUserId);
-      this.onFilterUpdate(this.mode);
+    //   this.onFilterUpdate(this.mode);
     });
+
   }
 
   ionViewWillEnter() {
     this.isLoading = true;
+    this.isLoadingUsers = true;
     this.authService.fetchUsers()
         .subscribe(() => {
-      this.isLoading = false;
-    });
+          this.isLoadingUsers = false;
+        });
     this.favoriteService.fetchFavorites()
         .subscribe(() => {
 
         });
+
+    this.relativeUserIdsSubs = this.authService.userId.pipe(
+        take(1),
+        switchMap(myUserId => {
+          if (!myUserId) {
+            throw new Error('User not found!');
+          }
+          return this.http.get<string[]>(
+              `https://algapi.herokuapp.com/getbyid?userId=${myUserId}`
+          );
+        })
+    ).subscribe(relativeUserIds => {
+      this.isLoading = true;
+      this.usersSub = this.authService.users.subscribe(users => {
+        this.allUsers = users; // save all users for favorites
+        this.loadedUsers = users.filter((value: MyUser) => {
+          return relativeUserIds.includes(value.id);
+        });
+        this.onFilterUpdate(this.mode);
+      });
+    });
   }
 
   onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail> | string) {
     this.authService.userId.pipe(take(1)).subscribe(userId => {
-      if ((event instanceof CustomEvent && event.detail.value === 'all') || (!(event instanceof CustomEvent) && event === 'all')) {
+      if ((event instanceof CustomEvent && event.detail.value === 'all') ||
+          (!(event instanceof CustomEvent) && event === 'all')) {
         this.mode = 'all';
         this.relevantUsers = this.loadedUsers.filter(
             myUser => myUser.id !== userId
         );
-        this.listedLoadedUsers = this.relevantUsers;
       } else {
         this.mode = 'favorites';
-        this.relevantUsers = this.loadedUsers.filter(
+        this.relevantUsers = this.allUsers.filter(
             myUser => this.favArray.includes(myUser.id)
         );
-        this.listedLoadedUsers = this.relevantUsers;
       }
-      // console.log('listedLoadedPlaces', this.listedLoadedPlaces);
+      this.listSize = this.relevantUsers.length;
+      this.isLoading = false;
     });
   }
 
@@ -77,6 +100,9 @@ export class DiscoverPage implements OnInit, OnDestroy {
     }
     if (this.favoritesSub) {
       this.favoritesSub.unsubscribe();
+    }
+    if (this.relativeUserIdsSubs) {
+      this.relativeUserIdsSubs.unsubscribe();
     }
   }
 
--- src/app/questionnaires/questionnaire-1/questionnaire-1.page.ts
+++ src/app/questionnaires/questionnaire-1/questionnaire-1.page.ts
@@ -1,19 +1,22 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {FormControl, FormGroup , NgForm, Validators} from '@angular/forms';
+import {FormControl, FormGroup , Validators} from '@angular/forms';
 import {Subscription} from 'rxjs';
 import {ActivatedRoute, Router} from '@angular/router';
-import {AlertController, LoadingController, NavController} from '@ionic/angular';
+import {AlertController} from '@ionic/angular';
 import {AuthService} from '../../auth/auth.service';
-import {switchMap, take} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
 
 class Question {
   question: string;
   answers: {[key: number]: string};
+  id: string;
   constructor(
       question: string,
-      answers: {[key: number]: string}) {
+      answers: {[key: number]: string},
+      id: string) {
     this.question = question;
     this.answers = answers;
+    this.id = id;
   }
 }
 
@@ -39,7 +42,8 @@ export class Questionnaire1Page implements OnInit, OnDestroy {
   constructor(private route: ActivatedRoute,
               private router: Router,
               private authService: AuthService,
-              private alertCtrl: AlertController) {}
+              private alertCtrl: AlertController,
+              private http: HttpClient) {}
 
   ngOnInit() {
     this.isLoading = true;
@@ -70,21 +74,22 @@ export class Questionnaire1Page implements OnInit, OnDestroy {
     const defAns: {[key: number]: string} = { 0: 'Not critical', 1: 'To a reasonable extent', 2: 'very much' };
 
     this.questions.push(
-        new Question('How much do you care if your partner agrees to raise an animal in the apartment ?', defAns));
+        new Question('How much do you care if your partner agrees to raise an animal in the apartment ?', defAns, 'animal'));
     this.questions.push(
-        new Question('How much it will bother you that your partner smokes in the apartment ?', defAns));
+        new Question('How much it will bother you that your partner smokes in the apartment ?', defAns, 'smoke'));
     this.questions.push(
-        new Question('How much it will bother you that your partner will often host people in the apartment ?', defAns));
+        new Question('How much it will bother you that your partner will often host people in the apartment ?', defAns, 'host'));
     this.questions.push(
-        new Question('How much do you care if your partner has the same gender as yours ?', defAns));
+        new Question('How much do you care if your partner has the same gender as yours ?', defAns, 'gender'));
     this.questions.push(
-        new Question('How important it is for you that your partner maintains order and cleanliness in the apartment ?', defAns));
+        new Question('How important it is for you that your partner maintains order and cleanliness in the apartment ?', defAns, 'clean'));
     this.questions.push(
-        new Question('How important it is for you that your partner agrees to joint shopping in the apartment ?', defAns));
+        new Question('How important it is for you that your partner agrees to joint shopping in the apartment ?', defAns, 'shop'));
     this.questions.push(
-        new Question('How important it is for you that your partner keeps kosher in the apartment ?', defAns));
+        new Question('How important it is for you that your partner keeps kosher in the apartment ?', defAns, 'kosher'));
     this.questions.push(
-        new Question('How important it is to you that your roommate be a vegetarian ?', defAns));
+        new Question('How important it is to you that your roommate be a vegetarian ?', defAns, 'vegetarian'));
+
 
     this.form = new FormGroup({});
     this.questions.forEach(value => {
@@ -105,18 +110,35 @@ export class Questionnaire1Page implements OnInit, OnDestroy {
   onSubmit() {
 
     // TODO send object value to algo then go to discover
-    const value = {userid: this.userId, myAnswers: this.myAnswers};
-    console.log(value);
+    const arr: {answer, question, id}[] = [];
+    // console.log('myAnswers', this.myAnswers);
+    for (const question of Object.keys(this.myAnswers)) {
+      // console.log('question', question);
+      const id: string = this.questions.find(value1 => {
+        // console.log('value1', value1);
+        return value1.question === question;
+      }).id;
+      arr.push({answer: this.myAnswers[question], question, id });
+    }
+    // console.log('arr', arr);
 
-    this.isLoading = false;
-    this.router.navigateByUrl('/places/tabs/discover');
+    const value = {userid: this.userId, myAnswers: arr};
+    console.log(value); // THIS OBJECT TO SEND!!!!
+
+    this.http.post<any>('https://algapi.herokuapp.com/getuser', value)
+        .subscribe(() => {
+          this.isLoading = false;
+          this.router.navigateByUrl('/places/tabs/discover');
+        }, error => {
+          console.log('error', error);
+        });
   }
 
   radioGroupChange(question: string, answer: string) {
-    console.log('question', question);
-    console.log('answer', answer);
+    // console.log('question', question);
+    // console.log('answer', answer);
     this.myAnswers[question] = answer;
-    console.log(this.myAnswers);
+    // console.log(this.myAnswers);
     this.myAnsLen = Object.keys(this.myAnswers).length;
   }
 }
